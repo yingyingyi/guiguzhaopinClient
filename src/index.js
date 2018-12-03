0f4a514a0d2233fa4a052fbce4fe32c54a213262
ying <ying@163.com>
2018-12-03 15:34:14 +0800

    客户端基本配置

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {HashRouter, Switch, Route} from 'react-router-dom';
+import {Provider} from 'react-redux';
+
+import store from './redux/store';
+
+import Login from './components/login';
+import Register from './components/register';
+import Main from './components/main';
+
+ReactDOM.render((
+    <Provider store={store}>
+    <HashRouter>
+    <Switch>
+    <Route path="/login" component={Login}/>
+    <Route path="/register" component={Register}/>
+    <Route path="/" component={Main}/>
+    </Switch>
+    </HashRouter>
+    </Provider>
+), document.getElementById('app'));
